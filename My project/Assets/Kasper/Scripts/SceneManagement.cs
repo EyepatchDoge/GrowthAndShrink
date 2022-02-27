751b309a21c7226eaedd6ef53ba612f7f1e503ad
EyepatchDoge <kaspo.eyepatch@gmail.com>
2022-02-27 12:36:41 -0500

    Created script for Scene Management
    -created simple script that can be use to change scene, pause and play the game

---
+++ My project/Assets/Kasper/Scripts/SceneManagement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+  public void ChangeScene(string sceneName)
+    {
+     
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void PlayGame()
+    {
+        Time.timeScale = 1;
+    }
+}
